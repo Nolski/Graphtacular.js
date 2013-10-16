8685e1663beffd2c042b69dcd0398309646e3a90
mpn3712 <mpn3712@rit.edu>
2013-10-16 17:24:38 -0400

    added simple bar graph display

--- graphtacular.js
+++ graphtacular.js
@@ -1,11 +1,23 @@
-function Graphtastic () {
-	this.labels = [];
-	this.values = [];
-	this.data = {};
+function Graphtacular (context, options) {
+    this._context = context;
+    this.data = (options.data || []);
+    this.bar_padding = 10;
+    console.log(context);
+    this.animate();
+    this.__defineSetter__("data", function(val){
+        console.log('setting data to: ' + val);
+        data = val;
+        this.animate();
+    });
 }
 
-Graphtastic.prototype = {
-	draw: function () {
-
-	}
+Graphtacular.prototype.animate = function (label, value) {
+    var bar_width = (this._context.canvas.width / this.data.length) - this.bar_padding;
+        x = this.bar_padding;
+    this._context.fillStyle = "blue";
+    for (var i = 0; i < this.data.length; i++) {
+        var bar = this.data[i];
+        this._context.fillRect(x, 100, bar_width - this.bar_padding, 50);
+        x += bar_width + this.bar_padding;
+    };
 }
