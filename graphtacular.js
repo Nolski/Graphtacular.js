99da13ae310ab3cb24336c617efbc150dca7f969
ExplosiveHippo <lancealaughlin@gmail.com>
2013-10-18 11:13:04 -0400

    Graph heights are now dynamic...upside down though

--- graphtacular.js
+++ graphtacular.js
@@ -2,7 +2,7 @@ function Graphtacular (context, options) {
     this._context = context;
     this.data = (options.data || []);
     this.bar_padding = 10;
-    console.log(context);
+    //console.log(context);
     this.animate();
 }
 
@@ -10,11 +10,13 @@ Graphtacular.prototype.animate = function (label, value) {
     canvas.width = canvas.width;
     var bar_width = (this._context.canvas.width / this.data.length) - this.bar_padding;
         x = this.bar_padding;
-    console.log(bar_width);
+    //console.log(bar_width);
     this._context.fillStyle = "blue";
     for (var i = 0; i < this.data.length; i++) {
         var bar = this.data[i];
-        this._context.fillRect(x, 100, bar_width - this.bar_padding, 50);
+        var height = bar[Object.keys(bar)[0]];
+        var bar_height = this._context.canvas.height * (height / 100);
+        this._context.fillRect(x, 200, bar_width - this.bar_padding, bar_height);
         x += bar_width + this.bar_padding;
     };
 }
